e923f3c8d14d9fe0dbf55a91957c35f28c5178a7
Sayuk <sayukbohdan@gmail.com>
2023-01-05 21:56:17 +0200

    S

--- src/src/js/01-gallery.js
+++ src/src/js/01-gallery.js
@@ -1,5 +1,42 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// // Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+// // Change code below this line
+const imageContainer = document.querySelector(".gallery");
+const imgMarkup = createImgGallery(galleryItems);
+imageContainer.insertAdjacentHTML("beforeend",imgMarkup );
+
+function createImgGallery(galleryItems){
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `<li class="gallery__item">
+        <a class="gallery__item" href="${original}" data-lightbox="lbox" onclick="return false">
+            <img 
+            class="gallery__image"
+            loading="lazy"
+            src='${preview}'
+            data-source="${original}"
+            alt="${description}"
+            
+            >
+            </a>
+            </li>
+        `
+    }).join("");
+}
+
+
+var gallery = new SimpleLightbox('.gallery a',{captionsData: 'alt', captionDelay: 250, });
+
+    gallery.on('show.simplelightbox', function (){
+});
+
 
 console.log(galleryItems);
+
+function n () {
+    console.log("Hi") 
+}
